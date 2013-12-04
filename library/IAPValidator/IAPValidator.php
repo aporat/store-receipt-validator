e009f73450099f4f832d0004153be9100f65f019
Adar Porat <hadar@mybuilding.org>
2013-12-04 14:05:26 -0500

    adding setter / getter functions

--- library/IAPValidator/IAPValidator.php
+++ library/IAPValidator/IAPValidator.php
@@ -1,26 +1,119 @@
 <?php
 namespace IAPValidator;
 
-class IAPValidator {
+use Guzzle\Http\Client;
 
-    const ENVIRONMENT_SANDBOX_URL = 'https://sandbox.itunes.apple.com/verifyReceipt';
-    const ENVIRONMENT_PRODUCTION_URL = 'https://buy.itunes.apple.com/verifyReceipt';
-    
-    
+class IAPValidator
+{
+
+    const ENDPOINT_SANDBOX = 'https://sandbox.itunes.apple.com/verifyReceipt';
+
+    const ENDPOINT_PRODUCTION = 'https://buy.itunes.apple.com/verifyReceipt';
+
     /**
-     * environment url
+     * endpoint url
+     * 
      * @var string
      */
-    protected $_environmentUrl;
+    protected $_endpoint;
 
-    public function __construct($environmentUrl = ENVIRONMENT_PRODUCTION_URL)
+    /**
+     * itunes receipt data, in base64 format
+     * 
+     * @var string
+     */
+    protected $_receiptData;
+
+    public function __construct($endpoint = ENDPOINT_PRODUCTION)
     {
-        if ($environmentUrl != self::ENVIRONMENT_PRODUCTION_URL && $environmentUrl != self::ENVIRONMENT_SANDBOX_URL) {
-            throw new RunTimeException(
-                "Invalid environment url '{$environmentUrl}'"
-            );
+        if ($endpoint != self::ENDPOINT_PRODUCTION && $endpoint != self::ENDPOINT_SANDBOX) {
+            throw new RunTimeException("Invalid endpoint '{$endpoint}'");
         }
         
-        $this->_environmentUrl = $environmentUrl;
+        $this->_endpoint = $endpoint;
     }
+
+    /**
+     * get receipt data
+     *
+     * @return string
+     */
+    public function getReceiptData()
+    {
+        return $this->_receiptData;
+    }
+
+    /**
+     * set receipt data, either in base64, or in json
+     *
+     * @param string $receiptData            
+     * @return \IAPValidator\IAPValidator
+     */
+    function setReceiptData($receiptData)
+    {
+        if (strpos($receiptData, '{') !== false) {
+            $this->_receiptData = base64_encode($receiptData);
+        } else {
+            $this->_receiptData = $receiptData;
+        }
+        
+        return $this;
+    }
+
+    /**
+     * get endpoint
+     *
+     * @return string
+     */
+    public function getEndpoint()
+    {
+        return $this->_endpoint;
+    }
+
+    /**
+     * set endpoint
+     *
+     * @param unknown $endpoint            
+     * @return \IAPValidator\IAPValidator
+     */
+    function setEndpoint($endpoint)
+    {
+        $this->_endpoint = $endpoint;
+        
+        return $this;
+    }
+
+    /**
+     * returns the Guzzle client
+     *
+     * @return \Guzzle\Http\Client
+     */
+    protected function getClient()
+    {
+        if ($this->_client == null) {
+            $this->_client = new Client($this->_environmentUrl);
+        }
+        
+        return $this->_client;
+    }
+
+    /**
+     * encode the request in json
+     *
+     * @return string
+     */
+    private function encodeRequest()
+    {
+        return json_encode(array(
+            'receipt-data' => $this->getReceiptData()
+        ));
+    }
+
+    private function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    public function validate($receiptData)
+    {}
 }
--- tests/IAPValidatorTest.php
+++ tests/IAPValidatorTest.php
@@ -2,25 +2,39 @@
 use IAPValidator\IAPValidator;
 
 /**
- *
  * @group library
  */
 class IAPValidatorTest extends PHPUnit_Framework_TestCase
 {
+
+    private $validator;
     
     public function setUp()
     {
         parent::setUp();
-    
-        $this->validator = new IAPValidator(IAPValidator::ENVIRONMENT_SANDBOX_URL);
+        
+        $this->validator = new IAPValidator(IAPValidator::ENDPOINT_SANDBOX);
     }
-    
+
     public function testInvalidOptionsToConstructor()
     {
-        $this->setExpectedException(
-                "IAPValidator\\RuntimeException", "Invalid environment url 'in-valid'"
-        );
+        $this->setExpectedException("IAPValidator\\RuntimeException", "Invalid endpoint 'in-valid'");
         
         $validator = new IAPValidator('in-valid');
+    }
+    
+    
+    public function testSetEndpoint()
+    {
+        $this->validator->setEndpoint(IAPValidator::ENDPOINT_PRODUCTION);
+        
+        $this->assertEquals(IAPValidator::ENDPOINT_PRODUCTION, $this->validator->getEndpoint());
+    }
+    
+    public function testSetReceiptData()
+    {
+        $this->validator->setReceiptData('test-data');
+    
+        $this->assertEquals('test-data', $this->validator->getReceiptData());
     }
 }
