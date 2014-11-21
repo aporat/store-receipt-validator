df5f9230630c9ddebcf5abefc21c3897de88d462
Adar Porat <aporat@users.noreply.github.com>
2014-11-21 09:28:02 -0500

    added comments to itunes response codes

--- src/ReceiptValidator/iTunes/Response.php
+++ src/ReceiptValidator/iTunes/Response.php
@@ -13,22 +13,32 @@ class Response
      */
     const RESULT_OK = 0;
 
+    // The App Store could not read the JSON object you provided.
     const RESULT_APPSTORE_CANNOT_READ = 21000;
 
+    // The data in the receipt-data property was malformed or missing.
     const RESULT_DATA_MALFORMED = 21002;
 
+    // The receipt could not be authenticated.
     const RESULT_RECEIPT_NOT_AUTHENTICATED = 21003;
 
+    // The shared secret you provided does not match the shared secret on file for your account.
+    // Only returned for iOS 6 style transaction receipts for auto-renewable subscriptions.
     const RESULT_SHARED_SECRET_NOT_MATCH = 21004;
 
+    // The receipt server is not currently available.
     const RESULT_RECEIPT_SERVER_UNAVAILABLE = 21005;
 
+    // This receipt is valid but the subscription has expired. When this status code is returned to your server, the receipt data is also decoded and returned as part of the response.
+    // Only returned for iOS 6 style transaction receipts for auto-renewable subscriptions.
     const RESULT_RECEIPT_VALID_BUT_SUB_EXPIRED = 21006;
 
+    // This receipt is from the test environment, but it was sent to the production environment for verification. Send it to the test environment instead.
     // special case for app review handling - forward any request that is intended for the Sandbox but was sent to Production, this is what the app review team does
     const RESULT_SANDBOX_RECEIPT_SENT_TO_PRODUCTION = 21007;
 
-    const RESULT_PRODUCTION_RECEIPT_SENT_TO_SENDBOX = 21008;
+    // This receipt is from the production environment, but it was sent to the test environment for verification. Send it to the production environment instead.
+    const RESULT_PRODUCTION_RECEIPT_SENT_TO_SANDBOX = 21008;
 
     /**
      * Result Code
