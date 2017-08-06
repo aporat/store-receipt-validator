267026be21f0430eb82854fcbf47fe2021a0a0a8
Niels Mouthaan <niels@backbase.com>
2017-08-06 15:09:09 +0200

    Added support for new "pending_renewal_info" fields (see https://developer.apple.com/news/?id=07182017a)

--- tests/iTunes/ResponseTest.php
+++ tests/iTunes/ResponseTest.php
@@ -53,5 +53,8 @@ class iTunesResponseTest extends PHPUnit_Framework_TestCase
 
     $this->assertInternalType(PHPUnit_Framework_Constraint_IsType::TYPE_STRING, $response->getBundleId());
     $this->assertEquals($jsonResponseArray['receipt']['bundle_id'], $response->getBundleId(), 'receipt bundle id must match');
+
+    $this->assertInternalType(PHPUnit_Framework_Constraint_IsType::TYPE_ARRAY, $response->getPendingRenewalInfo());
+    $this->assertEquals($jsonResponseArray['pending_renewal_info'], $response->getPendingRenewalInfo(), 'pending renewal info must match');
   }
 }
